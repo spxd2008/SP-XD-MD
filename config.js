ae7a82bbe19c72555c2ea8ddd628016092562708
spxd-08 <sadaruwanp426@gmail.com>
2025-12-17 21:11:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-};
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/s9c6sg.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello User i'm alive now!","};
